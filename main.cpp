c4a20438e15896833a298b87f981cffc9f0247c5
abishekmahendran <91681257+abishekmahendran@users.noreply.github.com>
2021-11-14 23:46:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+   int n, a=1, b=1,c;
+    cin >> n ;
+    if(n<2){
+        cout << "errore" <<endl;
+    }else{
+        for(int i=2; i<=n; i++){
+            c=a+b;
+            cout << c <<endl;
+            a=b;
+            b=c;
+
+        }
+    }
    return 0;
 }
